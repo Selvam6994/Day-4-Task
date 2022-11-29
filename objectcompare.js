cda8c8a7b4ea4edc5f8a333205c5e0d8311a4633
Selvam <107210789+Selvam6994@users.noreply.github.com>
2022-11-29 06:50:37 +0530

    Update objectcompare.js

--- objectcompare.js
+++ objectcompare.js
@@ -20,6 +20,18 @@ const isEqual = function (obj1, obj2) {
 };
 console.log(isEqual(obj1, obj2));
 
+/2.display countries flag.
+var request = new XMLHttpRequest();
+request.open("GET","https://restcountries.com/v3.1/all");
+request.send();
+request.onload=function(){
+    var rawdata=(request.response);
+    var data = JSON.parse(rawdata);
+    for(let i=0;i<data.length;i++){
+        console.log("png:"+data[i].flags.png,"svg:"+data[i].flags.svg)
+    }
+}
+
 
 
 
